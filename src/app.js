4e16e1e61bd94490c5ee5bea04151742fcfcf0fd
Sblaise123 <127683538+Sblaise123@users.noreply.github.com>
2023-05-20 01:23:56 +0000

    DomainNames

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let alldomains = [];
+let pronoun = ["they", "he", "she", "our"];
+let adj = ["little", "small", "big", "great"];
+let noun = ["raccoon", "runner", "dog", "bug"];
+let ends = [".org", ".com", ".edu"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const domainName = () => {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        for (let e = 0; e < ends.length; e++)
+          alldomains.push(pronoun[i] + adj[a] + noun[b] + ends[e]);
+      }
+    }
+  }
+  return alldomains;
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+console.log(domainName());
+
+window.onload = () => {
+  const domainNames = domainName();
+  const domainNameContainer = document.getElementById("domain-names");
+  domainNameContainer.innerHTML = domainNames.join("<br>");
 };
